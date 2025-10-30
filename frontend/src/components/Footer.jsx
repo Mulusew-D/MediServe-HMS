4759f0cbfcbabac0d1387eb4e9bcefb6d979acf4
Mulusew Desale <mulusewdesale@gmail.com>
2025-10-30 16:01:53 +0300

    initial commit
    Updated footer component to simplify hours display.

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,23 +4,16 @@ import { FaLocationArrow, FaPhone } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 
 const Footer = () => {
-  const hours = [
-    { id: 1, day: "Monday", time: "9:00 AM - 11:00 PM" },
-    { id: 2, day: "Tuesday", time: "12:00 PM - 12:00 AM" },
-    { id: 3, day: "Wednesday", time: "10:00 AM - 10:00 PM" },
-    { id: 4, day: "Thursday", time: "9:00 AM - 9:00 PM" },
-    { id: 5, day: "Friday", time: "3:00 PM - 9:00 PM" },
-    { id: 6, day: "Saturday", time: "9:00 AM - 3:00 PM" },
-  ];
-
   return (
     <footer className="footer container">
       <hr />
       <div className="footer-content">
+        {/* Logo */}
         <div className="footer-logo">
           <img src="/logo.png" alt="logo" className="logo-img" />
         </div>
 
+        {/* Quick Links */}
         <div className="footer-links">
           <h4>Quick Links</h4>
           <ul>
@@ -30,17 +23,13 @@ const Footer = () => {
           </ul>
         </div>
 
+        {/* Hours */}
         <div className="footer-hours">
-          <h4>Hours</h4>
-          <ul>
-            {hours.map((h) => (
-              <li key={h.id}>
-                <span>{h.day}:</span> <span>{h.time}</span>
-              </li>
-            ))}
-          </ul>
+          <h4>Opening Hours</h4>
+          <p>24 Hours</p>
         </div>
 
+        {/* Contact */}
         <div className="footer-contact">
           <h4>Contact</h4>
           <div className="contact-item">
@@ -60,6 +49,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
+
       <p className="footer-copy">&copy; 2025 MediServe. All rights reserved.</p>
     </footer>
   );
